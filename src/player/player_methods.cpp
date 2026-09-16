03d9d8dc0b264f687acb02527b8584aafb87797f
progress-ux <nikita15933@mail.ru>
2026-09-16 20:10:28 +0300

    respawm: add vtable for respawm hook

---
+++ src/player/player_methods.cpp
@@ -0,0 +1,31 @@
+#include "player_methods.h"
+
+#include "logger.h"
+
+namespace 
+{
+    constexpr std::size_t ROUND_RESPAWN_VTABLE_INDEX = 84;
+
+    using RoundRespawnFn = void (*)(void*);
+}
+
+namespace PlayerMethods
+{
+    void RoundRespawn(edict_t *pEntity)
+    {
+        if (!pEntity || !pEntity->pvPrivateData)
+        {
+            LH_ERROR("[PlayerMethods] [RoundRespawn] Invalid player/private data");
+            return;
+        }
+
+        void* player = pEntity->pvPrivateData;
+        void** vtable = *reinterpret_cast<void***>(player);
+
+        auto fn = reinterpret_cast<RoundRespawnFn>(
+            vtable[ROUND_RESPAWN_VTABLE_INDEX]
+        );
+
+        fn(player);
+    }
+}
---
+++ src/player/player_methods.h
@@ -0,0 +1,8 @@
+#pragma once 
+
+#include <extdll.h>
+
+namespace PlayerMethods
+{
+    void RoundRespawn(edict_t* pEntity);
+}
